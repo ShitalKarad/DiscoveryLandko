96dbe2b8ee4e4efd50126e5f7e062ae1c3a32d2f
ShitalKarad <shitalkarad24@gmail.com>
2023-09-08 14:48:05 +0530

    Refactor:created post api using jQuery added data on json server

--- script/employee-form.js
+++ script/employee-form.js
@@ -1,8 +1,6 @@
 function addElement() {
-    console.log("hello");
     const name = document.getElementById("name").value;
-    console.log(name);
-
+    
     var profiles = document.getElementsByName("profile");
     let selectedProfiles = "";
     for (let i = 0; i <= profiles.length; i++) {
@@ -11,8 +9,7 @@ function addElement() {
             break
         }
     }
-    console.log(selectedProfiles);
-
+    
     var gender = document.getElementsByName("gender");
     let selectedGender = "";
     for (let i = 0; i < gender.length; i++) {
@@ -21,27 +18,47 @@ function addElement() {
             break
         }
     }
-    console.log(selectedGender);
-
+    
     var departments = document.getElementsByName("department");
     let selectedDepartments = ""
     for (let i = 0; i < departments.length; i++) {
         if (departments[i].checked) {
             selectedDepartments += departments[i].value +" " ;
-        }
-        
+        } 
     }
-    console.log(selectedDepartments);
+    
     const salary = document.getElementById("salary").value
-    console.log(salary);
+    
 
     let day = document.getElementById("day").value;
     let month = document.getElementById("month").value;
     let year = document.getElementById("year").value;
-    const date = day+"-"+month+"-"+"year";
-    console.log(date);
-    
+    const date = day+"-"+month+"-"+year;
     const note = document.getElementById("note").value;
-    console.log(note);
 
+    const userInput = {
+        "fName":name,
+        "profile": selectedProfiles,
+        "gender":selectedGender,
+        "department":selectedDepartments,
+        "salary":salary,
+        "date":date,
+        "note":note
+    }
+
+    console.log(userInput);
+    
+
+    $.ajax({
+        url:"http://localhost:3000/posts",
+        type:"post",
+        dataType:"json",
+        data:userInput,
+        success:function (data) {
+            console.log(data);
+        },
+        error:function(error) {
+            console.log(error);
+        }
+    })
 }
